a36bd51cde9d11d5b78cff883d1beded6aa1c7cf
Villa Myo <myatmyo299@gmail.com>
2020-09-03 11:06:28 +0630

    Add some transaction object.

--- personal_expense/lib/main.dart
+++ personal_expense/lib/main.dart
@@ -33,8 +33,20 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
-  final List<Transaction> transactions = [];
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 12.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 19.99,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
